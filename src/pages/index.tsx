dafddf5fb1953d600d4c03d33638e66a780fecb6
over2000 <danielvitorcg@gmail.com>
2022-09-01 02:29:28 -0300

    feat: langin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from 'next/head'
-import styles from 'styles/home.module.scss'
-import { ColorModeSwitcher } from '../components/coloSwitcher'
 
-export default function Home() {
+import { useRouter } from 'next/router'
+
+import styles from 'styles/home.module.scss'
+
+
+const Home = () => {
+
+  const router = useRouter()
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Next</title>
       </Head>
-      <ColorModeSwitcher />
+    <h1>tela inicial</h1>
+    <button type="button" onClick={() => router.push('/login')}>
+      login
+    </button>
     </div>
+
   )
 }
+
+export default Home
