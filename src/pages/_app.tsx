af058950f0380b91f6a97a16e517acfa99a9b6b7
over2000 <danielvitorcg@gmail.com>
2022-09-01 02:28:55 -0300

    refactor: remove theme context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,45 +1,11 @@
-import 'styles/globals.scss'
-import { SessionProvider } from 'next-auth/react'
-import { MantineProvider, ColorSchemeProvider } from '@mantine/core'
-import { getCookie, setCookies } from 'cookies-next'
-import { useState } from 'react'
-
-export default function App(props) {
-  const {
-    Component,
-    pageProps: { session, ...pageProps }
-  } = props
-
-  const [colorScheme, setColorScheme] = useState(props.colorScheme)
-
-  const toggleColorScheme = value => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
-    setColorScheme(nextColorScheme)
-    setCookies('mantine-color-scheme', nextColorScheme, {
-      maxAge: 60 * 60 * 24 * 30
-    })
-  }
-
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <>
-      <SessionProvider session={session}>
-        <ColorSchemeProvider
-          colorScheme={colorScheme}
-          toggleColorScheme={toggleColorScheme}
-        >
-          <MantineProvider
-            theme={{ colorScheme }}
-            withGlobalStyles
-            withNormalizeCSS
-          >
-            <Component {...pageProps} />
-          </MantineProvider>
-        </ColorSchemeProvider>
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   )
 }
-
-App.getInitialProps = ({ ctx }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'n '
-})
