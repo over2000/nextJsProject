2c2ae08e3a8ac56cb034f530c63c9bd10cc9d477
over2000 <danielvitorcg@gmail.com>
2022-09-20 16:22:57 -0300

    wip: navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,47 +9,40 @@ function Navbar() {
         <a href='#'>Empty</a>
       </h1>
       <ul className={`main-nav ${!session && loading ? 'loading' : 'loaded'}`}>
-        <li>
-          <Link href='/'>
-            <a>Home</a>
-          </Link>
-        </li>
-        {/* <li>
-          <Link href='/dashboard'>
-            <a>Dashboard</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/blog'>
-            <a>Blog</a>
-          </Link>
-        </li> */}
-
         {!loading && !session && (
           <li>
             <Link href='/api/auth/signin'>
               <a
-                onClick={e => {
+                onClick={(e) => {
                   e.preventDefault()
                   signIn('github')
-                }}>
+                }}
+              >
                 Sign In
               </a>
             </Link>
           </li>
         )}
         {session && (
-          <li>
-            <Link href='/api/auth/signout'>
-              <a
-                onClick={e => {
-                  e.preventDefault()
-                  signOut()
-                }}>
-                Sign Out
-              </a>
-            </Link>
-          </li>
+          <>
+            <li>
+              <Link href='/dashboard'>
+                <a>Dashboard</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/api/auth/signout'>
+                <a
+                  onClick={(e) => {
+                    e.preventDefault()
+                    signOut()
+                  }}
+                >
+                  Sign Out
+                </a>
+              </Link>
+            </li>
+          </>
         )}
       </ul>
     </nav>
