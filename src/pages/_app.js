1b47c038ece7722d51940b43308e26cb35e5546d
over2000 <danielvitorcg@gmail.com>
2022-09-22 16:36:57 -0300

    wip: add context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@ import { Provider } from 'next-auth/client'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import '../components/Navbar.css'
+import { AppWrapper } from '../context/state'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <Navbar />
-      <Component {...pageProps} />
+      <AppWrapper>
+        <Navbar />
+        <Component {...pageProps} />
+      </AppWrapper>
     </Provider>
   )
 }
